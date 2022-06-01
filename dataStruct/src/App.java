809b7e8d8fe0b1b609893f64c08d0e1290604a22
Dansiego-0 <bomber8183@gmail.com>
2022-06-01 23:56:09 +0800

    idhjsiad

--- dataStruct/src/App.java
+++ dataStruct/src/App.java
@@ -33,14 +33,6 @@ public class App {
           }
           return isFound;
     }
-    public static int Search(int [][]Alpha, int []prev) {
-        for (int x = 0; x <= Alpha.length-1; x++) {
-            for (int y = 0; y<=Alpha[x].length-1;y++) {
-                  if (Alpha[x][y] == prev[0] && Alpha[x][y+1] == prev[1]) return x; 
-            }
-          }
-          return -1;
-    }
 
     public static void Copy(int A[][], int b[][]) {
         for (int x = 0; x < A.length; x++) {
@@ -48,11 +40,6 @@ public class App {
                 b[x][y] = A[x][y];
             }
         }
-    }
-
-    public static void DifferenceOfSets(int [][]Alpha, int [][]prev, int [][]next) {
-        // A −B = {x |x ∈A ∧x 6∈B }
-        System.out.println(Alpha.length);
     }
 
     public static void Replace(int Alpha[][], int prev[], int next[]) {
@@ -65,7 +52,6 @@ public class App {
             }
           }
     }
-
 
     public static void Delete(int Alpha[][], int prev[]) {
         int size = Alpha.length;
@@ -104,10 +90,11 @@ public class App {
             System.out.println("\n");
         }
     }
-    public static void NextPlayerMove(int []prev, int []next, boolean aTurn, int [][]Alpha, int [][]Beta, int [][]Free ,int [][]P, boolean ok, int [][]S, int [] cardinalities) {
-
+    public static void NextPlayerMove(int []prev, int []next, int [][]Alpha, int [][]Beta, int [][]Free ,int [][]P, int [][]S, int [] cardinalities) {
+        boolean ok = false;
+        boolean aTurn = true;
         Scanner s = new Scanner(System.in);
-        if (!aTurn) System.out.println("Alpha's Turn!");
+        if (aTurn == true) System.out.println("Alpha's Turn!");
         else System.out.println("Beta's Turn!");
         System.out.println("Enter coordinates!");
         System.out.println("Which Piece would you like to move?");
@@ -128,90 +115,20 @@ public class App {
         ViewPoints(prev);
         System.out.print("To be moved at: ");
         ViewPoints(next);
-
+        // Prev[0] = 6 prev[1] = 2 next[0] = 5 next[1] = 2
+        //2 == 2 && 5 == 7 && 2 == 3
         if (isAnElement(Alpha, prev) && aTurn && prev[0] == next[0]+1 && (next[1] == prev[1] && next[1] == prev[1] + 1 && prev[1] == next[1] + 1)) ok = !ok;
         else if (isAnElement(Beta, prev) && !aTurn && next[0] == prev[0]+1 && (next[1] == prev[1] && next[1] == prev[1] + 1 && prev[1] == next[1] + 1)) ok = !ok;
-        else ok = true;
-
-        if (ok && aTurn && isAnElement(Free, next)) {
+    
+        if (aTurn && isAnElement(Free, next)) {
             Replace(Alpha, prev, next);
-            aTurn = false;
-            ok = !ok;
-        }
-        if (ok && !aTurn && isAnElement(Free, next)) {
-            Replace(Beta, prev, next);
-            aTurn = true;
-            ok = !ok;
-        }
-
-         if (ok && aTurn && isAnElement(Beta, next) && !isAnElement(S, prev)){
-             ok = !ok;
-             System.out.println("Shesh6");
-        }else if (ok && aTurn && isAnElement(Beta, next) && isAnElement(S, prev)) {
-            Delete(Beta, next);
-            Replace(Alpha,prev,next);
             aTurn = !aTurn;
-            ok = !ok;
-            cardinalities[1]--;
-            System.out.println("Shesh3");
-        } 
-
-        if (ok && !aTurn && !isAnElement(Alpha, prev)) {
-            ok = !ok;
-            System.out.println("Shesh5");
-        }else if (ok && !aTurn && isAnElement(Alpha, prev)) {
-            Delete(Alpha, next);
-            Replace(Beta,prev,next);
-            aTurn = !aTurn;
-            ok = !ok;
-            cardinalities[0]--;
-            System.out.println("Shesh4");
+            System.out.println(aTurn);
         }
-        
+    
 
     }
 
-
-    public static void PutPiece(int R, int C, int prev[]) {
-        for (int row = 0; row < 5; row++) {
-            for (int column = 0; column < 7 ; column++) {
-                if (row == prev[0] && column == prev[1]) {
-                    System.out.print("X\t");
-                }else System.out.print("*\t");
-            }
-            System.out.println();
-        }
-        System.out.println(prev[0] + " " + prev[1]);
-    }
-
-    public static boolean Checker(int [][]P, int [][]Union, int check) {
-        int checker = 0;
-        boolean go = false;
-        while (true && checker < 35) {
-            try {
-                if (P[checker][0] == Union[check][0]) {
-                    if (P[checker][1] == Union[check][1]) {
-                        go = true;
-                        // System.out.println("P = {" + P[checker][0] + "," + P[checker][1] +"}" + "," + "U = {" + Union[check][0] + "," + Union[check][1] + "}");
-                        checker = 0;
-                        break;
-                    }
-                }
-            }catch (Exception e) {
-                if (P[checker][0] == Union[9][0]) {
-                    if (P[checker][1] == Union[9][1]) {
-                        go = true;
-                        // System.out.println("P = {" + P[checker][0] + "," + P[checker][1] +"}" + "," + "U = {" + Union[9][0] + "," + Union[9][1] + "}");
-                        checker = 0;
-                        break;
-                    }
-                }
-            }
-            if (check >= 10) break;
-            checker++;
-        }
-        return go;
-    }
 
     public static boolean CheckerV2(int [][]P, int [][]Union, int check, int row) {
         boolean isFound = false;
@@ -227,7 +144,7 @@ public class App {
     public static void ViewBoard(int A[][], int B[][], int [] cardinalities, int P[][]) {
         int [][]Union = new int[cardinalities[0] + cardinalities[1]][2];
         int check = 0;
-        int alphaCounter = 0, betaCounter = 0; 
+        int betaCounter = 0; 
         
 
         SetUnion(B, A, Union);
@@ -238,13 +155,16 @@ public class App {
                 System.out.println();
                 newLine = 0;
             }
-            if (CheckerV2(P, Union, check, row)) {
-                if (betaCounter < cardinalities[1]) System.out.print("B\t");
-                else System.out.print("A\t");
-                check++;
-                betaCounter++;
+            // if (CheckerV2(P, Union, check, row)) {
+            //     if (betaCounter < cardinalities[1]) System.out.print("B\t");
+            //     else System.out.print("A\t");
+            //     check++;
+            //     betaCounter++;
+            // }else System.out.print("*\t");
+            // if (check >= 10) check = 9;
+            if (CheckerV2(P, Union, 2, row)) {
+                System.out.print("B\t");
             }else System.out.print("*\t");
-            if (check >= 10) check = 9;
             
         }
         System.out.println();
@@ -255,12 +175,9 @@ public class App {
     public static void GameOver(){
         
     }
+
     public static void main(String[] args) throws Exception {
         int []cardinalities = {5,5};
-        int R = 7; // Dynamic array
-        int C = 5; // Dynamic array
-        int prev[] = new int[2];
-        Scanner sc = new Scanner(System.in);
         int[][]P = {
             {1,1},{1,2},{1,3},{1,4},{1,5},
             {2,1},{2,2},{2,3},{2,4},{2,5},
@@ -270,6 +187,7 @@ public class App {
             {6,1},{6,2},{6,3},{6,4},{6,5},
             {7,1},{7,2},{7,3},{7,4},{7,5}
         };
+
         int[][] S = {
             {1,1},{1,3},{1,5},{2,2},{2,4},{3,1},{3,3},{3,5},{4,2},{4,4},{5,1},{5,3},{5,5},{6,2},{6,4},{7,1},{7,3},{7,5}
         };
@@ -283,17 +201,15 @@ public class App {
         };
     
         int [][] Beta = {
-            {1,1},
+            {2,1},
             {1,3},
             {1,5},
             {2,2},
             {2,4}
         };
+
         int [][] Free = {
             {1,2},{1,4},{2,1},{2,3},{2,5},{3,1},{3,2},{3,3},{3,4},{3,5},{4,1},{4,2},{4,3},{4,4},{4,5},{5,1},{5,2},{5,3},{5,4},{5,5},{6,1},{6,3},{6,5},{7,2},{7,4}
-        };
-        int alphaBetaU[][] = {
-            {6,2},{6,4},{7,1},{7,3},{7,5},{1,1},{1,3},{1,5},{2,2},{2,4}
         };
 
 
@@ -304,30 +220,50 @@ public class App {
         boolean aTurn = true;
         int []move = new int[2];
         int []pieceToMove = new int[2];
-        move[0] = 5;
-        move[1] = 2; 
-
-        pieceToMove[0] = 6;
-        pieceToMove[1] = 2;
+        
         
         Welcome();
         ViewBoard(Alpha, Beta, cardinalities, P);
-        Replace(Alpha, pieceToMove, move); // Kaya iba output dahil dito dito oo tignan mo ung FindplayerNextMove sa taas dun gaganapin lahat
-        System.out.println();
-        ViewSet(Alpha);// bat wala inaask sa user pag nirun ko? di pa ba na iimplement yung user inp
+        // while(true) {
+        //     System.out.println("Cardinalities: " + cardinalities[0] + "-" + cardinalities[1]);
+        //     ViewBoard(Alpha, Beta, cardinalities, P);
+        //     Scanner s = new Scanner(System.in);
+        //     if (aTurn == true) System.out.println("Alpha's Turn!");
+        //     else System.out.println("Beta's Turn!");
+        //     System.out.println("Enter coordinates!");
+        //     System.out.println("Which Piece would you like to move?");
+        //     System.out.println("Enter point A: ");
+        //     pieceToMove[0] = s.nextInt();
+        //     System.out.println("Enter point B: ");
+        //     pieceToMove[1] = s.nextInt();
     
-        ViewBoard(Alpha, Beta, cardinalities, P);
+        //     System.out.println("Where would you like to move it?");
+        //     System.out.println("Enter point C: ");
+        //     move[0] = s.nextInt();
+        //     System.out.println("Enter point D:");
+        //     move[1] = s.nextInt();
+    
+    
+        //     System.out.print("Points to be moved: ");
+        //     ViewPoints(pieceToMove);
+        //     System.out.print("To be moved at: ");
+        //     ViewPoints(move);
+
+        //     if (isAnElement(Alpha, pieceToMove) && aTurn && pieceToMove[0] == move[0]+1 && (move[1] == pieceToMove[1] && move[1] == pieceToMove[1] + 1 && pieceToMove[1] == move[1] + 1)) ok = !ok;
+        //     else if (isAnElement(Beta, pieceToMove) && !aTurn && move[0] == pieceToMove[0]+1 && (move[1] == pieceToMove[1] && move[1] == pieceToMove[1] + 1 && pieceToMove[1] == move[1] + 1)) ok = !ok;
         
-        // Checker(Alpha,P);
-        // while (true) {
-        //     ViewBoard(Alpha, Beta, cardinalities);
-        //     NextPlayerMove(move, pieceToMove, aTurn, Alpha, Beta, Free, P, ok, S, cardinalities);
+        //     if (!ok && aTurn && isAnElement(Free, move)) {
+        //         Replace(Alpha, pieceToMove, move);
+        //         aTurn = !aTurn;
+        //         System.out.println(aTurn);
+        //         ok = !ok;
+        //     }else if (ok && !aTurn && isAnElement(Free, move)) {
+        //         Replace(Beta, pieceToMove, move);
+        //         aTurn = !aTurn;
+        //         System.out.println("HELLLO PUTANGINAMO");
+        //         ok = !ok;
+        //     }
         // }
         
-
-        
-
-        
-
     }
 }
